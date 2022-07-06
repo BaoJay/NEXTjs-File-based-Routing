9a6ba66d9ab94e5ffc76799a06faa01e16ce3aec
Gia Bao Nguyen Hoang <giabao.nguyenhoang1001@gmail.com>
2022-07-06 10:39:57 +0700

    finish

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,14 +2,29 @@ import { useRouter } from "next/router";
 import { Fragment } from "react";
 import EventList from "../../components/events/EventList";
 import { getFilteredEvents } from "../../dummy_data";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/Button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 const FilteredPage = () => {
   const router = useRouter();
   //   const someId = router.query["some-id"];
   //   const getData = getStaticProps () =>{}
 
-  if (!router.query.slug || router.query.slug.length > 2) {
-    return <p>Nothing was found!</p>;
+  // Encounter the first time page is rendered
+  if (!router.query.slug) {
+    return <p className="center">Loading....</p>;
+  } else if (router.query.slug.length > 2) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p className="center">Nothing was found!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
   const [year, month, ...restId] = router.query.slug;
   console.log(router.query.slug.length);
@@ -24,17 +39,24 @@ const FilteredPage = () => {
   const filteredData = getFilteredEvents(selectedDate);
   console.log(filteredData);
 
-  if (filteredData.length === 0) {
+  const date = new Date(year, month - 1);
+
+  if (!filteredData || filteredData.length === 0) {
     return (
       <Fragment>
-        <h3>No event was matched!</h3>
+        <ErrorAlert>
+          <p className="center">No event was matched for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
       </Fragment>
     );
   } else {
     return (
       <Fragment>
+        <ResultsTitle date={date} />
         <EventList items={filteredData} />
-        {/* <h2>Hello Temporary!</h2> */}
       </Fragment>
     );
   }
